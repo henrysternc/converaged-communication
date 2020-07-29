2bac1e146e10f2ae9d1b56b028cbe9edb0f77b15
shizhiheng <shizhiheng@atgco.cn>
2020-07-29 14:55:28 +0800

    修改报错

--- src/main/java/test/stone/communication/ConvergedCommunicationApplication.java
+++ src/main/java/test/stone/communication/ConvergedCommunicationApplication.java
@@ -1,7 +1,6 @@
 package test.stone.communication;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -13,12 +12,9 @@ import test.stone.communication.netty.client.NettyClient;
 @MapperScan(basePackages = "test.stone.communication.dao")
 public class ConvergedCommunicationApplication {
 
-    @Autowired
-    private static ObuServerHandler obuServerHandler;
-
     public static void main(String[] args) {
         SpringApplication.run(ConvergedCommunicationApplication.class, args);
-        NettyClient nettyClient = new NettyClient("127.0.0.1", 49161);
+        NettyClient nettyClient = new NettyClient();
         nettyClient.connect();
     }
 }
